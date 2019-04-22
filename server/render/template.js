a10daaf05a9b04202c8e878ae9d61420a4b2610b
yurkagon <owngameplay@gmail.com>
2019-04-22 12:18:05 +0300

    [FIX] styles

--- server/render/template.js
+++ server/render/template.js
@@ -7,12 +7,12 @@ export default (reactDom) => `
         <meta charset="utf-8">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <title>React Koa SSR</title>
+        <style>${StyleHandler.getStyleData()}</style>
     </head>
 
     <body>
         <div id="root">${reactDom}</div>
         <script src="main.js"></script>
-        <style>${StyleHandler.getStyleData()}</style>
     </body>
   </html>
 `
